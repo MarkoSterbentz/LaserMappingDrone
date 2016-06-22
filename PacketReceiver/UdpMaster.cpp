1a0e29552c86f85edecc75675a69405a6c631231
Galen Cochrane <cochgale@isu.edu>
2016-06-22 17:58:38 -0600

    progress on UdpMaster

--- PacketReceiver/UdpMaster.cpp
+++ PacketReceiver/UdpMaster.cpp
@@ -10,12 +10,16 @@ namespace LaserMappingDrone {
     int UdpMaster::needyMasterCount = 0;
 
     UdpMaster::UdpMaster() {
-        hasInitialized = false;
+
     }
 
     UdpMaster::~UdpMaster() {
         freeResources();
         quitSdlNetIfUnused();
+    }
+
+    bool UdpMaster::init() {
+        return false;
     }
 
     bool UdpMaster::initListener(const std::string &localIp, uint16_t localPort, uint32_t packetSize) {
@@ -30,25 +34,18 @@ namespace LaserMappingDrone {
                                    uint16_t localPort,
                                    uint32_t packetSize)
     {
-        initSdlNetIfNeeded();
-        if (hasInitialized) {
-            freeResources();
-        }
-        if (!openPort(localPort)) {
+        freeResources();
+        initSdlNetIfNotDone();
+        if (!openLocalSocket(localPort)) {
             return false;
         }
-        if (!setUpPort(remoteIp, remotePort)) {
+        if (!openRemoteSocket(remoteIp, remotePort)) {
             return false;
         }
-        if (!allocPacket(packetSize)) {
-            return false;
-        }
-
-        hasInitialized = true;
-        return true;
+        return setUpPacket(packetSize);
     }
 
-    bool UdpMaster::initSdlNetIfNeeded() {
+    bool UdpMaster::initSdlNetIfNotDone() {
         needyMasterCount += 1;
         if (!sdlNetHasInitialized) {
             if (SDLNet_Init() == -1) {
@@ -71,7 +68,7 @@ namespace LaserMappingDrone {
         return false;
     }
 
-    bool UdpMaster::allocPacket(uint32_t packetSize) {
+    bool UdpMaster::setUpPacket(uint32_t packetSize) {
         // free old packet memory
         SDLNet_FreePacket(packet);
         // allocate new packet memory
@@ -80,22 +77,22 @@ namespace LaserMappingDrone {
             std::cout << "\tSDLNet_AllocPacket failed : " << SDLNet_GetError() << std::endl;
             return false;
         }
-        packet->address.host = serverIP.host;
-        packet->address.port = serverIP.port;
+        packet->address.host = remoteSocket.host;
+        packet->address.port = remoteSocket.port;
         return true;
     }
 
-    bool UdpMaster::openPort(uint16_t port) {
-        socket = SDLNet_UDP_Open(port);
-        if (!socket) {
-            std::cout << "Failed to open socket: " << SDLNet_GetError() << std::endl;
+    bool UdpMaster::openLocalSocket(uint16_t port) {
+        localSocket = SDLNet_UDP_Open(port);
+        if (!localSocket) {
+            std::cout << "Failed to open local socket: " << SDLNet_GetError() << std::endl;
             return false;
         }
         return true;
     }
 
-    bool UdpMaster::setUpPort(const std::string &ip, uint16_t port) {
-        if (SDLNet_ResolveHost(&serverIP, ip.c_str(), port) == -1) {
+    bool UdpMaster::openRemoteSocket(const std::string &host, uint16_t port) {
+        if (SDLNet_ResolveHost(&remoteSocket, host.c_str(), port) == -1) {
             std::cout << "Failed to resolve host: " << SDLNet_GetError() << std::endl;
             return false;
         }
@@ -104,22 +101,23 @@ namespace LaserMappingDrone {
 
     bool UdpMaster::send() {
         // returns number of packets sent - 0 is error.
-        if (SDLNet_UDP_Send(socket, -1, packet) == 0 ) {
+        if (SDLNet_UDP_Send(localSocket, -1, packet) == 0 ) {
             std::cout << "Failed to send packet: " << SDLNet_GetError() << "\n";
             return false;
         }
         return true;
     }
 
-    void UdpMaster::recv()
+    void UdpMaster::receiveOne()
     {
-        if (SDLNet_UDP_Recv(socket, packet)) {
+        if (SDLNet_UDP_Recv(localSocket, packet)) {
             std::cout << "Packet received: " << packet->data << "\n";\
         }
     }
 
     void UdpMaster::freeResources() {
         SDLNet_FreePacket(packet);
+        packet = NULL;
     }
 
 }
--- PacketReceiver/UdpMaster.h
+++ PacketReceiver/UdpMaster.h
@@ -12,33 +12,41 @@
 #include "SDL_net.h"
 
 namespace LaserMappingDrone {
+
     class UdpMaster
     {
     public:
+
         UdpMaster();
         ~UdpMaster();
+
+        bool init();
+
+        bool send();
+        void receiveOne();
+
+    private:
+
+        static bool sdlNetHasInitialized;
+        static int needyMasterCount;
+
+        UDPsocket localSocket;
+        IPaddress remoteSocket;
+        UDPpacket *packet;
+        std::stringstream errorLog;
+
+        // todo: go into inheriting classes
         bool initListener(const std::string &localIp, uint16_t localPort, uint32_t packetSize);
         bool initSender(const std::string &remoteIp, uint16_t remotePort, uint32_t packetSize);
         bool initTwoWay(const std::string &remoteIp, uint16_t remotePort,
                         uint16_t localPort,
                         uint32_t packetSize);
-        bool send();
-        void recv();
 
-    private:
-        UDPsocket socket;
-        IPaddress serverIP;
-        UDPpacket *packet;
-
-        bool hasInitialized;
-        static bool sdlNetHasInitialized;
-        static int needyMasterCount;
-
-        bool initSdlNetIfNeeded();
+        bool initSdlNetIfNotDone();
         bool quitSdlNetIfUnused();
-        bool allocPacket(uint32_t packetSize);
-        bool openPort(uint16_t port);
-        bool setUpPort(const std::string &ip, uint16_t port);
+        bool setUpPacket(uint32_t packetSize);
+        bool openLocalSocket(uint16_t port);
+        bool openRemoteSocket(const std::string &host, uint16_t port);
         void freeResources();
     };
 
